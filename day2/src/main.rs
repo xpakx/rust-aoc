73df006e46e0aa001ee5e1cd7a44582cd840fd5e
xpakx <55586470+xpakx@users.noreply.github.com>
2023-05-16 18:17:30 +0200

    (day2): complete second star

--- day2/src/main.rs
+++ day2/src/main.rs
@@ -3,6 +3,11 @@ use std::io::{self, BufRead};
 use std::path::Path;
 
 fn main() {
+    first_star();
+    second_star();
+}
+
+fn first_star() {
     let file_path = Path::new("./input.txt");
     let file =  match File::open(&file_path){
         Ok(file) => file,
@@ -19,8 +24,6 @@ fn main() {
             let elve_choice = split.next();
             let strategy = split.next();
             if let (Some(elve), Some(strategy)) = (elve_choice, strategy) {
-                println!("Elve: {}", elve);
-                println!("Strategy: {}", strategy);
                 let shape_bonus = match strategy {
                     "X" => 1,
                     "Y" => 2,
@@ -41,5 +44,45 @@ fn main() {
         }
     }
     println!("Result: {}", result);
-    
+}
+
+fn second_star() {
+    let file_path = Path::new("./input.txt");
+    let file =  match File::open(&file_path){
+        Ok(file) => file,
+        Err(error) => {
+            eprintln!("Cannot load file: {}", error);
+            return;
+        }
+    };
+    let lines = io::BufReader::new(file).lines();
+    let mut result = 0;
+    for line in lines {
+        if let Ok(ln) = line {
+            let mut split = ln.split(" ");
+            let elve_choice = split.next();
+            let strategy = split.next();
+            if let (Some(elve), Some(strategy)) = (elve_choice, strategy) {
+                let shape_bonus = match (elve, strategy) {
+                    ("A", "X") => 3,
+                    ("A", "Y") => 1,
+                    ("A", "Z") => 2,
+                    ("B", "X") => 1,
+                    ("B", "Y") => 2,
+                    ("B", "Z") => 3,
+                    ("C", "X") => 2,
+                    ("C", "Y") => 3,
+                    ("C", "Z") => 1,
+                    (_, _) => 0
+                };
+                let result_bonus = match strategy {
+                    "Y" => 3,
+                    "Z" => 6,
+                    _ => 0
+                };
+                result = result + result_bonus + shape_bonus;
+            }
+        }
+    }
+    println!("Result: {}", result);
 }
