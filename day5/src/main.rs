edfbc7afc463c6615895a7ff19b338dc332dae0a
xpakx <55586470+xpakx@users.noreply.github.com>
2023-05-17 19:06:08 +0200

    (day5): complete first star

--- day5/src/main.rs
+++ day5/src/main.rs
@@ -39,28 +39,32 @@ fn main() {
             }
         }
     }   
-    for row in cargo {
-        for elem in row {
-            print!("{}", elem);
-        }
-        print!("\n");
-    }
     while let Some(line) = lines.next() {
         if let Ok(line) = line {
             let instruction = parse_instruction(&line);
-            println!("{}: {} -> {}", instruction.0, instruction.1, instruction.2);
+            for _ in 0..(instruction.0) {
+                let c = cargo.get_mut(instruction.1).unwrap().pop(); 
+                if let Some(c) = c {
+                    cargo.get_mut(instruction.2).unwrap().push(c);
+                }
+
+            }
         }
     }
+    for mut col in cargo {
+        print!("{}", match col.pop() {Some(c) => c, None => ' '});
+    }
+    print!("\n");
 }
 
-fn parse_instruction(text: &String) -> (u32, u32, u32) {
+fn parse_instruction(text: &String) -> (u32, usize, usize) {
     let mut elements = text.split(" ");
     let amount = elements.nth(1);
     let column_from = elements.nth(1);
     let column_to = elements.nth(1);
     if let (Some(a), Some(f), Some(t)) = (amount, column_from, column_to) {
-        if let(Ok(a), Ok(f), Ok(t)) = (a.parse::<u32>(), f.parse::<u32>(), t.parse::<u32>()) {
-            return (a,f,t);
+        if let(Ok(a), Ok(f), Ok(t)) = (a.parse::<u32>(), f.parse::<usize>(), t.parse::<usize>()) {
+            return (a,f-1,t-1);
         }
     }
     return (0, 0, 0);
