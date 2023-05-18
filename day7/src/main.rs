96456f4e7b7ac4b772170add4725cf35172379f2
xpakx <55586470+xpakx@users.noreply.github.com>
2023-05-18 13:06:30 +0200

    (day7): complete first star (stack-based solution)

--- day7/src/main.rs
+++ day7/src/main.rs
@@ -12,20 +12,43 @@ fn main() {
         }
     };
     let lines = io::BufReader::new(file).lines();
+    let mut stack = Vec::new();
+    let mut result = 0;
     for line in lines {
         if let Ok(line) = line {
-            println!("{}", line);
             let mut splitted = line.split(" ");
             let command = splitted.next().unwrap();
             let command = match command {
                 "$" => splitted.next().unwrap(),
                 x => x
             };
+            if command == "ls" || command == "dir" {
+                continue;
+            }
             let target = match command {
                 "cd" | "dir" => Some(splitted.next().unwrap()),
-                _ => None
+                _ => Some(splitted.next().unwrap())
             };
-
+            if command == "cd" {
+                let new_dir = target.unwrap();
+                if new_dir == ".." {
+                    if let Some(value) = stack.pop() {
+                        if value <= 100000 {
+                            result += value;
+                        }
+                        let last = stack.len()-1;
+                        stack[last] += value;
+                    }
+                } else {
+                    stack.push(0);
+                }
+            } else {
+                let file_size: usize = command.parse().unwrap();
+                let last = stack.len()-1;
+                stack[last] += file_size;
+            }
         }
     }
+    println!("{}", result);
 }
+
