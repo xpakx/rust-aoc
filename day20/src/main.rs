8c143224f6e6258d563ae18abdf413edd67a677f
xpakx <55586470+xpakx@users.noreply.github.com>
2023-05-28 18:58:31 +0200

    (day20): complete second star

--- day20/src/main.rs
+++ day20/src/main.rs
@@ -7,7 +7,7 @@ fn main() {
         .expect("Should read file")
         .into_iter()
         .filter_map(|a| a.ok())
-        .map(|a| a.parse::<i32>())
+        .map(|a| a.parse::<i64>())
         .filter_map(|a| a.ok())
         .collect();
 
@@ -18,8 +18,13 @@ fn main() {
 
 }
 
-fn first_star(numbers: &Vec<i32>) -> i32 {
-    let decoded = mix(&numbers);
+fn first_star(numbers: &Vec<i64>) -> i64 {
+    let indices: Vec<usize> = numbers
+        .iter()
+        .enumerate()
+        .map(|(i, _)| i)
+        .collect();
+    let decoded = mix(&numbers, &indices);
     let old_zero_index = numbers
         .iter()
         .position(|&a| a == 0)
@@ -38,23 +43,49 @@ fn first_star(numbers: &Vec<i32>) -> i32 {
         .sum()
 }
 
-fn second_star(_numbers: &Vec<i32>) -> i32 {
-    0
-}
+fn second_star(init_numbers: &Vec<i64>) -> i64 {
+    const DECRYPTION_KEY: i64 = 811589153;
+    let numbers: Vec<i64> = init_numbers
+        .iter()
+        .map(|a| a * DECRYPTION_KEY)
+        .collect();
 
-fn mix(numbers: &Vec<i32>) -> Vec<usize> {
     let mut indices: Vec<usize> = numbers
         .iter()
         .enumerate()
         .map(|(i, _)| i)
         .collect();
+    
+    for _ in 0..10 {
+        indices = mix(&numbers, &indices);
+    }
+    let old_zero_index = numbers
+        .iter()
+        .position(|&a| a == 0)
+        .unwrap();
+
+    let zero_index = indices
+        .iter()
+        .position(|&a| a == old_zero_index)
+        .unwrap();
+
+    [1000, 2000, 3000]
+        .iter()
+        .map(|a| (a + zero_index) % indices.len())
+        .map(|a| indices[a])
+        .map(|a| numbers[a])
+        .sum()
+}
+
+fn mix(numbers: &Vec<i64>, indices: &Vec<usize>) -> Vec<usize> {
+    let mut indices: Vec<usize> = indices.iter().map(|a| a.clone()).collect();
     for (i, n) in numbers.iter().enumerate() {
         let elem_index = indices
             .iter()
             .position(|a| a == &i)
             .unwrap();
         indices.remove(elem_index);
-        let new_index = (elem_index as i32 + n).rem_euclid(indices.len() as i32) as usize;
+        let new_index = (elem_index as i64 + n).rem_euclid(indices.len() as i64) as usize;
         indices.insert(new_index, i);
     }
     indices
