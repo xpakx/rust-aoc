091dab7fa4744cb0f0a5d97095358b60953bf175
xpakx <55586470+xpakx@users.noreply.github.com>
2023-05-30 16:35:23 +0200

    (day22): solve first star

--- day22/src/main.rs
+++ day22/src/main.rs
@@ -10,29 +10,61 @@ fn main() {
         .expect("Input should contain both map and moves");
     let instructions: Vec<Move> = parse_moves(moves);
     let map: Vec<Vec<Tile>> = parse_map(map);
-    println!("{:?}", instructions);
-    for row in map.iter() {
-        for tile in row.iter() {
-            print!(" {} ", match tile {
-                Tile::Nothing => ' ',
-                Tile::Wall => '#',
-                Tile::Floor => '.'
-            });
-        }
-        println!("");
-    }
-    let height = first_star();
-    println!("Height: {}", height);
+    let map2: Vec<Vec<Tile>> = append_empty_tiles(&map);
+    let start: (usize, usize) = get_starting_position(&map[0]).expect("First row should contain open position");
+    let height = first_star(&map2, &instructions, &start);
+    println!("First star: {}", height);
     let height = second_star();
-    println!("Height: {}", height);
+    println!("Second star: {}", height);
 }
 
-fn first_star() -> usize {
-    0
+fn first_star(map: &Vec<Vec<Tile>>, instructions: &Vec<Move>, start: &(usize, usize)) -> usize {
+    use Direction::*;
+    let mut position = start.clone();
+    let mut direction = Right;
+    for inst in instructions.iter() {
+        match inst {
+            Move::Rotate(rotation) => {
+                direction = direction.rotate(&rotation);
+            },
+            Move::Forward(distance) => {
+                position = find_achievable_position(&map, &position, distance, &direction);
+            },
+        }
+    }
+    
+    return (1 + position.0) * 1000 + (1 + position.1) * 4 + direction.value()
 }
 
 fn second_star() -> usize {
     0
+}
+
+fn find_achievable_position(map: &Vec<Vec<Tile>>, start: &(usize, usize), steps: &u32, direction: &Direction) -> (usize, usize) {
+    let mut candidate = start.clone();
+    for _ in 0..steps.clone() {
+        let mut pos = next_pos(&candidate, direction, map);
+        while let Tile::Nothing = map[pos.0][pos.1] {
+            pos = next_pos(&pos, direction, map);
+        }
+        match map[pos.0][pos.1] {
+            Tile::Wall => return candidate,
+            Tile::Nothing => {},
+            Tile::Floor => candidate = pos,
+        }
+    }
+
+    candidate
+}
+
+fn next_pos(position: &(usize, usize), direction: &Direction, map: &Vec<Vec<Tile>>) -> (usize, usize) {
+    use Direction::*;
+    match direction {
+        Down => ((position.0 + 1).rem_euclid(map.len()), position.1.clone()),
+        Up => ((position.0 as i32 - 1).rem_euclid(map.len() as i32) as usize, position.1.clone()),
+        Left => (position.0.clone(), (position.1 as i32 - 1).rem_euclid(map[position.0].len() as i32) as usize),
+        Right => (position.0.clone(), (position.1 + 1).rem_euclid(map[position.0].len())),
+    }
 }
 
 fn read_input() -> Result<String, io::Error> {
@@ -69,6 +101,7 @@ fn parse_moves(input: &str) -> Vec<Move> {
     }
     moves
 }
+
 fn parse_map(input: &str) -> Vec<Vec<Tile>> {
     let mut result = Vec::new();
     for line in input.lines() {
@@ -86,6 +119,29 @@ fn parse_map(input: &str) -> Vec<Vec<Tile>> {
     result
 }
 
+fn append_empty_tiles(map: &Vec<Vec<Tile>>) -> Vec<Vec<Tile>> {
+    let mut max_length = 0;
+    for row in map.iter() {
+        if row.len() > max_length {
+            max_length = row.len();
+        }
+    }
+    map
+        .iter()
+        .map(|row| {
+            let mut new_row: Vec<Tile> = row
+                .iter()
+                .map(|t| t.clone())
+                .collect();
+            if row.len() < max_length {
+                new_row.extend(vec![Tile::Nothing; max_length - row.len()]);
+            }
+            new_row
+        })
+    .collect()
+}
+
+#[derive(Clone)]
 enum Tile {
     Wall,
     Floor,
@@ -103,3 +159,54 @@ enum Rotation {
     Left,
     Right,
 } 
+
+enum Direction {
+    Up,
+    Down,
+    Left,
+    Right,
+}
+
+impl Direction {
+    fn value(&self) -> usize {
+        use Direction::*;
+        match self {
+            Right => 0,
+            Down => 1,
+            Left => 2,
+            Up => 3,
+        }
+    }
+
+    fn rotate(self, rotation: &Rotation) -> Direction {
+        use Direction::*;
+        match self {
+            Right => match rotation {
+                Rotation::Left => Up,
+                Rotation::Right => Down,
+            },
+            Down => match rotation {
+                Rotation::Left => Right,
+                Rotation::Right => Left,
+            },
+            Left => match rotation {
+                Rotation::Left => Down,
+                Rotation::Right => Up,
+            },
+            Up => match rotation {
+                Rotation::Left => Left,
+                Rotation::Right => Right,
+            },
+        }
+    }
+}
+
+fn get_starting_position(row: &Vec<Tile>) -> Option<(usize, usize)> {
+    for i in 0..row.len() {
+        if let Tile::Floor = row[i] {
+            return Some((0, i))
+        }
+    }
+
+    None
+}
